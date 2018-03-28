f493eba971f0d6cf5c80be5015c91ef236c23c7a
DoubleB123 <bbrenner321@gmail.com>
2018-03-27 18:24:13 -0700

    working on step 1

---
+++ src/components/CityInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './styles/styles.css' 
+
+class CityInput extends React.Component {
+  render() {
+    return (
+      <div className='city-input-container'>
+        <ul className='city-input-list'>
+          <li>
+            <h3 className='city-input-header'>Enter a City and State</h3>
+          </li>
+          <li>
+            <input className='city-input-input' type='text' placeholder='Houston, Texas'></input>
+          </li>
+          <li>
+            <button className='city-input-button'>Get Weather</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+export default CityInput
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/styles.css' 
+
+function Header(){ 
+  return (
+      <header className="main-header">
+        <h1 className="main-title">Header for Main.js</h1>
+      </header>
+  );
+}
+
+export default Header;
