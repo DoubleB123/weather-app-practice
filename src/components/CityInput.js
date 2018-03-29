7a0763dc7fdd8d10c1659b9830ce5b73e4ae1648
DoubleB123 <bbrenner321@gmail.com>
2018-03-28 17:52:49 -0700

    now button will print what you entered into the console

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -3,6 +3,25 @@ import './styles/CityInput.css'
 import PropTypes from 'prop-types';
 
 class CityInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      city: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      city: event.target.value 
+    })
+  }
+
+  handleSubmit(event) {
+    console.log('submited' + this.state.city);
+  }
+
   render() {
     return (
       <div>
@@ -11,10 +30,15 @@ class CityInput extends React.Component {
             <h3 className={'city-input-header-' + this.props.inputClass}>Enter a City and State</h3>
           </li>
           <li className={this.props.inputClass}>
-            <input className={'city-input-input-' + this.props.inputClass} type='text' placeholder='Houston, Texas'></input>
+            <input className={'city-input-input-' + this.props.inputClass} 
+              type='text' placeholder='Houston, Texas'
+              value={this.state.city} onChange={this.handleChange}>
+            </input>
           </li>
           <li className={this.props.inputClass}>
-            <button className={'city-input-button-' + this.props.inputClass}>Get Weather</button>
+            <button className={'city-input-button-' + this.props.inputClass}
+              onClick={this.handleSubmit} 
+            >Get Weather</button>
           </li>
         </ul>
       </div>
