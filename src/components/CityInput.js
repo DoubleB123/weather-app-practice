14bc6fb27608e7e5d6599f7036c9cf9c9d61712d
DoubleB123 <bbrenner321@gmail.com>
2018-03-29 17:17:00 -0700

    step 4 working

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './styles/CityInput.css' 
 import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const APIKEY = 'fe60cbb8648c979c422e7c0dc22ce3cf';
 
 class CityInput extends React.Component {
   constructor(props) {
@@ -19,7 +22,19 @@ class CityInput extends React.Component {
   }
 
   handleSubmit(event) {
-    console.log('submited' + this.state.city);
+    console.log('submited ' + this.state.city);
+    const currentWeatherStr = 'http://api.openweathermap.org/data/2.5/weather?zip=' + this.state.city +'&type=accurate&APPID=' + APIKEY;
+    const forecastStr = 'http://api.openweathermap.org/data/2.5/forecast?zip=' + this.state.city + '&APPID=' + APIKEY;
+    axios.get(currentWeatherStr)
+      .then(data => {
+        console.log(data);
+      });
+      /*
+    axios.get(forecastStr)
+      .then(data => {
+        console.log(data);
+      });
+      */
   }
 
   render() {
@@ -27,11 +42,11 @@ class CityInput extends React.Component {
       <div>
         <ul className={'city-input-list-' + this.props.inputClass}>
           <li className={this.props.inputClass}>
-            <h3 className={'city-input-header-' + this.props.inputClass}>Enter a City and State</h3>
+            <h3 className={'city-input-header-' + this.props.inputClass}>Enter a Zip Code</h3>
           </li>
           <li className={this.props.inputClass}>
             <input className={'city-input-input-' + this.props.inputClass} 
-              type='text' placeholder='Houston, Texas'
+              type='text' placeholder='90254'
               value={this.state.city} onChange={this.handleChange}>
             </input>
           </li>
